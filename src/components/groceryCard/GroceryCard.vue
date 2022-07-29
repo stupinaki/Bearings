a7d3887ad5b25af3c9cba9c989ace9b8ab9d7468
Ksenia Stupina <k.stupina@okenit.ru>
2022-07-29 17:19:34 +0300

    add GroceryCard

--- src/components/groceryCard/GroceryCard.vue
+++ src/components/groceryCard/GroceryCard.vue
@@ -1,80 +1,147 @@
 <template>
-  <div class="grocery">
+  <div :class="styled.grocery">
     <div>
-      <div>
+      <div :class="styled.description">
         <div>
-          <div>Маркировка</div>
-          <div>6302cg18</div>
+          <div :class="styled.descriptionTitle">Маркировка</div>
+          <div :class="styled.descriptionSubTitle">
+            {{ mark }}
+          </div>
         </div>
         <div>
-          <div>Цена</div>
-          <div>3.00 ₽/шт</div>
+          <div :class="styled.descriptionTitle">Цена</div>
+          <div :class="styled.descriptionSubTitle">{{ priceForOne }} ₽/шт</div>
         </div>
         <div>
-          <div>Наличие</div>
-          <div>320 шт</div>
+          <div :class="styled.descriptionTitle">Наличие</div>
+          <div :class="styled.descriptionSubTitle">{{ availability }} шт</div>
         </div>
       </div>
-      <div>
+
+      <div :class="styled.details">
         <div>
-          <div>
-            <div>Класс точности</div>
-            <div>6</div>
+          <div :class="styled.row">
+            <div :class="styled.detailsTitle">Класс точности</div>
+            <div :class="styled.detailsSubTitle">
+              {{ accuracyClass }}
+            </div>
           </div>
-          <div>
-            <div>Тип</div>
-            <div>6п</div>
+          <div :class="styled.row">
+            <div :class="styled.detailsTitle">Тип</div>
+            <div :class="styled.detailsSubTitle">
+              {{ type }}
+            </div>
           </div>
-          <div>
-            <div>Параметр</div>
-            <div>889</div>
+          <div :class="styled.row">
+            <div :class="styled.detailsTitle">Параметр</div>
+            <div :class="styled.detailsSubTitle">
+              {{ parameter }}
+            </div>
           </div>
-          <div>
-            <div>Производитель</div>
-            <div>ГПЗ 4</div>
+          <div :class="styled.row">
+            <div :class="styled.detailsTitle">Производитель</div>
+            <div :class="styled.detailsSubTitle">
+              {{ manufacturer }}
+            </div>
           </div>
         </div>
 
         <div>
-          <div>
-            <div>Внутренний диаметр (мм)</div>
-            <div>5.0</div>
+          <div :class="styled.row">
+            <div :class="styled.detailsTitle">Внутренний диаметр (мм)</div>
+            <div :class="styled.detailsSubTitle">
+              {{ innerDiameter }}
+            </div>
           </div>
-          <div>
-            <div>Внешний диаметр (мм)</div>
-            <div>19.0</div>
+          <div :class="styled.row">
+            <div :class="styled.detailsTitle">Внешний диаметр (мм)</div>
+            <div :class="styled.detailsSubTitle">
+              {{ externalDiameter }}
+            </div>
           </div>
-          <div>
-            <div>Ширина (мм)</div>
-            <div>6.0</div>
+          <div :class="styled.row">
+            <div :class="styled.detailsTitle">Ширина (мм)</div>
+            <div :class="styled.detailsSubTitle">
+              {{ width }}
+            </div>
           </div>
-          <div>
-            <div>Масса (кг)</div>
-            <div>0.009</div>
+          <div :class="styled.row">
+            <div :class="styled.detailsTitle">Масса (кг)</div>
+            <div :class="styled.detailsSubTitle">
+              {{ weight }}
+            </div>
           </div>
         </div>
       </div>
     </div>
 
-    <GetInTouchCard />
+    <div v-for="card in getInTouchCardsValue" :key="card.id">
+      <GetInTouchCard v-bind="card" />
+    </div>
   </div>
 </template>
 
 <script>
 import styled from "./groceryCard.module.css";
 import GetInTouchCard from "../getInTouchCard/GetInTouchCard.vue";
+import { getInTouchCardsValue } from "../../../data/getInTouchCardsValue";
 
 export default {
   name: "GroceryCard",
   data() {
     return {
       styled,
+      getInTouchCardsValue,
     };
   },
   components: {
     GetInTouchCard,
   },
+  props: {
+    mark: {
+      type: String,
+      default: "-",
+    },
+    priceForOne: {
+      type: String,
+      default: "-",
+    },
+    availability: {
+      type: String,
+      default: "-",
+    },
+    accuracyClass: {
+      type: String,
+      default: "-",
+    },
+    type: {
+      type: String,
+      default: "-",
+    },
+    parameter: {
+      type: String,
+      default: "-",
+    },
+    manufacturer: {
+      type: String,
+      default: "-",
+    },
+    innerDiameter: {
+      type: String,
+      default: "-",
+    },
+    externalDiameter: {
+      type: String,
+      default: "-",
+    },
+    width: {
+      type: String,
+      default: "-",
+    },
+    weight: {
+      type: String,
+      default: "-",
+    },
+  },
 };
 </script>
-
-<style scoped></style>
