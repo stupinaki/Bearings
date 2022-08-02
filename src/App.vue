3a3c331f279b208b71308d7b4dcafc4a00dee3a6
Ksenia Stupina <k.stupina@okenit.ru>
2022-08-02 10:46:38 +0300

    resolve conflict

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,12 @@
   <v-app>
     <v-main>
       <GroceryCard />
-      <div v-for="card in getInTouchCardsValue" :key="card.id">
-        <GetInTouchCard v-bind="card" />
-      </div>
       <CompanyCards />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import GetInTouchCard from "./components/getInTouchCard/GetInTouchCard.vue";
 import { getInTouchCardsValue } from "../data/getInTouchCardsValue";
 import CompanyCards from "./components/companyCards/CompanyCards.vue";
 import GroceryCard from "./components/groceryCard/GroceryCard.vue";
@@ -26,21 +22,9 @@ export default {
     };
   },
   components: {
-    GetInTouchCard,
     CompanyCards,
     GroceryCard,
   },
 };
 </script>
 
-<style>
-:root {
-  --primary-color: #4a53f5;
-  --primary-hover-color: #3840e6;
-  --pseudo-hover-color: #edeeff;
-  --secondary-color: #f3f5ff;
-  --secondary-hover-color: #d8ddf9;
-  --secondary-gray-color: #3a3d4b;
-  --white-color: #f4f6f8;
-}
-</style>
