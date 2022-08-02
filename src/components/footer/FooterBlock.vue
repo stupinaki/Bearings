1019793827f06d2da2cbaf3f05613f3818edafb3
Ksenia Stupina <k.stupina@okenit.ru>
2022-08-02 11:47:16 +0300

    fix comments

--- src/components/footer/FooterBlock.vue
+++ src/components/footer/FooterBlock.vue
@@ -5,23 +5,23 @@
         <div :class="styled.columnLogo">PIC</div>
       </div>
       <div :class="styled.column">
-        <a :class="styled.columnItem">
+        <a :class="styled.columnItem" href="#">
           <b>Покупателям</b>
         </a>
-        <a :class="styled.columnItem"> Найти подшибники </a>
-        <a :class="styled.columnItem"> Оптовый заказ </a>
-        <a :class="styled.columnItem"> Помощь </a>
-        <a :class="styled.columnItem"> Вопросы и ответы </a>
+        <a :class="styled.columnItem" href="#"> Найти подшибники </a>
+        <a :class="styled.columnItem" href="#"> Оптовый заказ </a>
+        <a :class="styled.columnItem" href="#"> Помощь </a>
+        <a :class="styled.columnItem" href="#"> Вопросы и ответы </a>
       </div>
       <div :class="styled.column">
-        <a :class="styled.columnItem">
+        <a :class="styled.columnItem" href="#">
           <b>Продавцам</b>
         </a>
-        <a :class="styled.columnItem"> Сотрудничество </a>
-        <a :class="styled.columnItem"> Вопросы и ответы </a>
+        <a :class="styled.columnItem" href="#"> Сотрудничество </a>
+        <a :class="styled.columnItem" href="#"> Вопросы и ответы </a>
       </div>
       <div :class="styled.column">
-        <a :class="styled.columnItem">
+        <a :class="styled.columnItem" href="#">
           <b>Контакты</b>
         </a>
         <a href="tel:79272772330" :class="styled.columnItem">
@@ -31,44 +31,47 @@
           skovalev74@yandex.ru
         </a>
         <div :class="styled.columnItem">
-          <a :class="styled.linksItem">
-            <img src="../../../public/imgs/footer/vk.svg" alt="vk" />
+          <a :class="styled.linksItem" href="#">
+            <img :src="Vk" alt="vk" />
           </a>
-          <a :class="styled.linksItem">
-            <img
-              src="../../../public/imgs/footer/facebook.svg"
-              alt="facebook"
-            />
+          <a :class="styled.linksItem" href="#">
+            <img :src="Facebook" alt="facebook"/>
           </a>
-          <a :class="styled.linksItem">
-            <img
-              src="../../../public/imgs/footer/instagram.svg"
-              alt="instagram"
-            />
+          <a :class="styled.linksItem" href="#">
+            <img :src="Instagram" alt="instagram"/>
           </a>
-          <a :class="styled.linksItem">
-            <img src="../../../public/imgs/footer/twitter.svg" alt="twitter" />
+          <a :class="styled.linksItem" href="#">
+            <img :src="Twitter" alt="twitter" />
           </a>
         </div>
       </div>
     </div>
 
     <div>
-      <a :class="styled.bottomItem"> Политика конфиденциальности </a>
-      <a :class="styled.bottomItem"> Договор оферты </a>
-      <a :class="styled.bottomItem"> Обратная связь </a>
+      <a :class="styled.bottomItem" href="#"> Политика конфиденциальности </a>
+      <a :class="styled.bottomItem" href="#"> Договор оферты </a>
+      <a :class="styled.bottomItem" href="#"> Обратная связь </a>
     </div>
   </div>
 </template>
 
 <script>
 import styled from "./footerBlock.module.css";
+import Twitter from "../../assets/twitter.svg";
+import Instagram from "../../assets/instagram.svg";
+import Facebook from "../../assets/facebook.svg";
+import Vk from "../../assets/vk.svg";
+
 
 export default {
   name: "FooterBlock",
   data() {
     return {
       styled,
+      Twitter,
+      Instagram,
+      Facebook,
+      Vk,
     };
   },
 };
