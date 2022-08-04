9d059fb693c34858150114b1feab57d6a9648dff
Ksenia Stupina <k.stupina@okenit.ru>
2022-08-04 14:44:27 +0300

    use module

--- src/components/typography/TypographyText.vue
+++ src/components/typography/TypographyText.vue
@@ -3,26 +3,23 @@
 </template>
 
 <script>
-import styled from "./typography.css";
+import styled from "./typography.module.css";
 
 export default {
   name: "TypographyText",
   props: {
     size: {
       type: String,
-      default: "xxm",
+      default: "subTitle1",
       validator(size) {
         return [
-          "xxs",
-          "xs",
-          "s",
-          "xxm",
-          "xm",
-          "m",
-          "xxxl",
-          "xxl",
-          "xl",
-          "l",
+          "subTitle2",
+          "subTitle1",
+          "title2",
+          "title1",
+          "header3",
+          "header2",
+          "header1",
         ].includes(size);
       },
     },
@@ -37,14 +34,9 @@ export default {
       type: String,
       default: "darkgrey",
       validator(type) {
-        return [
-          "darkgray",
-          "darkblue",
-          "white",
-          "gray",
-          "lightgray",
-          "coral",
-        ].includes(type);
+        return ["darkgray", "darkblue", "white", "gray", "coral"].includes(
+            type
+        );
       },
     },
   },
@@ -56,10 +48,10 @@ export default {
   computed: {
     className() {
       const { size, font, color } = this.$props;
-      const classes = ["typography"];
-      classes.push(`size-${size}`);
-      classes.push(`font-${font}`);
-      classes.push(`color-${color}`);
+      const classes = [styled.typography];
+      classes.push(size);
+      classes.push(styled[`font-${font}`]);
+      classes.push(styled[`color-${color}`]);
 
       return classes.filter((v) => v).join(" ");
     },
