6ee099cebe2e74a00effad8e4aa5a99575e25dfa
Ksenia Stupina <k.stupina@okenit.ru>
2022-08-04 13:21:12 +0300

    delete import svg

--- data/benefitsValue.js
+++ data/benefitsValue.js
@@ -1,28 +1,20 @@
-import Search from "../src/assets/search.svg";
-import Chat from "../src/assets/chat.svg";
-import Cart from "../src/assets/cart.svg";
-
-
 export const benefitsValue = [
   {
     id: 11771,
     title: "Бесплатно для покупателей",
     subTitle: "Мы бесплатно помогаем найти подшипники по выгодной цене.",
     imgName: "search",
-    way: Search,
   },
   {
     id: 11552,
     title: "Без посредников",
     subTitle: "Сможете напрямую связаться с заводами-производителями.",
     imgName: "chat",
-    way: Chat,
   },
   {
     id: 11663,
     title: "Оптовая закупка",
     subTitle: "Есть возможность оптовой закупки напрямую у поставщков.",
     imgName: "cart",
-    way: Cart,
   },
 ];
--- src/components/benefitsCards/BenefitsCards.vue
+++ src/components/benefitsCards/BenefitsCards.vue
@@ -7,11 +7,7 @@
       <BenefitsCard
         :title="benefit.title"
         :sub-title="benefit.subTitle"
-        :way="benefit.way"
       >
-        <Search />
-        <Chat />
-        <Cart />
       </BenefitsCard>
     </div>
   </div>
@@ -22,17 +18,10 @@ import BenefitsCard from "../benefitsCard/BenefitsCard.vue";
 import { benefitsValue } from "../../../data/benefitsValue";
 import styled from "./benefitsCards.module.css";
 
-import Search from "../../assets/search.svg";
-import Chat from "../../assets/chat.svg";
-import Cart from "../../assets/cart.svg";
-
 export default {
   name: "BenefitsCards",
   components: {
     BenefitsCard,
-    Search,
-    Chat,
-    Cart,
   },
   data() {
     return {
