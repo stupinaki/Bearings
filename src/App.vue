86c72b97ad0d0a7fdce3a232a13936e04f8677c6
Ksenia Stupina <k.stupina@okenit.ru>
2022-08-15 15:15:50 +0300

    removed the example with global styles from app

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@
         @dragleave.prevent
       >
         <HeaderBlock />
-        <GlobalStyleExample />
         <router-view />
         <FooterBlock />
       </div>
@@ -22,14 +21,12 @@ import styles from "./styles/app.module.css";
 import reset from "../src/styles/reset.css"
 import HeaderBlock from "@/components/header/HeaderBlock.vue";
 import FooterBlock from "@/components/footer/FooterBlock.vue";
-import GlobalStyleExample from "./components/globalStyleExample/GlobalStyleExample.vue";
 
 export default {
   name: "App",
   components: {
     HeaderBlock,
     FooterBlock,
-    GlobalStyleExample,
   },
   data() {
     return {
