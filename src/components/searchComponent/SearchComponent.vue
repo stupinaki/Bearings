d221410a5365f6fca6f97cf2d900fef1cea992dd
Ksenia Stupina <k.stupina@okenit.ru>
2022-08-31 09:57:04 +0300

    BEAR-36 replace header

--- src/components/searchComponent/SearchComponent.vue
+++ src/components/searchComponent/SearchComponent.vue
@@ -1,4 +1,12 @@
 <template>
+  <div :class="styled.header">
+    <div :class="styled.title">
+      Сервис по поиску подшипников
+    </div>
+    <div :class="styled.subTitle">
+      Мы не продаем подшипники, а помогаем найти лучшие предложения по низкой цене.
+    </div>
+  </div>
   <form
     :class="styled.wrapper"
   >
--- src/pages/homePage/HomePage.vue
+++ src/pages/homePage/HomePage.vue
@@ -1,13 +1,5 @@
 <template>
   <div :class="styled.container">
-    <div :class="styled.header">
-      <div :class="styled.title">
-        Сервис по поиску подшипников
-      </div>
-      <div :class="styled.subTitle">
-        Мы не продаем подшипники, а помогаем найти лучшие предложения по низкой цене.
-      </div>
-    </div>
     <SearchComponent />
     <BenefitsCards />
     <BulkOrderForm />
