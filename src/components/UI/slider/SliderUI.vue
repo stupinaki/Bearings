a725a0f2dbc6990f8ec835786dae1f45809aee8a
Ksenia Stupina <k.stupina@okenit.ru>
2022-09-15 13:16:10 +0300

    BEAR-42 merge main, use var for breakpoints

--- src/components/UI/slider/SliderUI.vue
+++ src/components/UI/slider/SliderUI.vue
@@ -7,9 +7,7 @@
     >
       <ArrowImg :class="styled.arrowPrev" />
     </ButtonUI>
-
     <slot :items="currentItems" />
-
     <ButtonUI
       type-style="pseudo"
       :class="btnNextStyle"
@@ -22,6 +20,8 @@
 
 <script>
 import chunk from "lodash/chunk";
+import {breakpoints} from "../../../consts/breakpoints";
+import {mapState} from "vuex";
 import ArrowImg from "../../../assets/iconForward.svg";
 import ButtonUI from "../button/ButtonUI.vue";
 import styled from "./sliderUI.module.css";
@@ -48,20 +48,23 @@ export default {
     return {
       styled,
       chunkNumber: 0,
-      windowInnerWidth: undefined,
     }
   },
   computed: {
+    ...mapState("viewport", ["viewportWidth"]),
     visibleItems() {
       const {visibleItemsMaxCount, items} = this.$props;
-      if(this.$data.windowInnerWidth < 1200 && this.$data.windowInnerWidth > 900) {
+      if (this.viewportWidth < breakpoints.extraLarge && this.viewportWidth > breakpoints.large) {
         return chunk(items, 4);
       }
-      if(this.$data.windowInnerWidth <= 900 && this.$data.windowInnerWidth > 700) {
+      if (this.viewportWidth <= breakpoints.large && this.viewportWidth > breakpoints.small) {
         return chunk(items, 3);
       }
-      if(this.$data.windowInnerWidth <= 700) {
+      if (this.viewportWidth <= breakpoints.small && this.viewportWidth > breakpoints.extraSmall) {
         return chunk(items, 2);
+      }
+      if (this.viewportWidth <= breakpoints.extraSmall) {
+        return chunk(items, 1);
       }
       return chunk(items, visibleItemsMaxCount);
     },
@@ -69,36 +72,33 @@ export default {
       return this.visibleItems[this.$data.chunkNumber];
     },
     btnNextStyle() {
-      if(this.visibleItems.length === 1) {
+      if (this.visibleItems.length === 1) {
         return [styled.btnWrapper, styled.btnHide];
       }
-      if(this.$data.chunkNumber < this.visibleItems.length - 1) {
+      if (this.$data.chunkNumber < this.visibleItems.length - 1) {
         return [styled.btnWrapper, styled.btnVisible];
       }
       return [styled.btnWrapper, styled.btnHide];
     },
     btnPrevStyle() {
-      if(this.visibleItems.length === 1) {
+      if (this.visibleItems.length === 1) {
         return [styled.btnWrapper, styled.btnHide];
       }
-      if(this.$data.chunkNumber > 0) {
+      if (this.$data.chunkNumber > 0) {
         return [styled.btnWrapper, styled.btnVisible];
       }
       return [styled.btnWrapper, styled.btnHide];
     }
   },
-  beforeMount() {
-    this.$data.windowInnerWidth = window.innerWidth;
-  },
   methods: {
     goNext() {
-      if(this.$data.chunkNumber < this.visibleItems.length - 1) {
-        return this.$data.chunkNumber++
+      if (this.$data.chunkNumber < this.visibleItems.length - 1) {
+        return this.$data.chunkNumber++;
       }
     },
     goPrev() {
-      if(this.$data.chunkNumber > 0) {
-        return this.$data.chunkNumber--
+      if (this.$data.chunkNumber > 0) {
+        return this.$data.chunkNumber--;
       }
     }
   }
